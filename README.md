aad3ec36d28247ceba2aa0ac584ea98527da8e1b
Jonas Schell <jonasschell@ocupe.org>
2018-08-30 01:01:12 +0200

    checkbox status is now saved in rethinkdb

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 websockets application to check and uncheck checkboxes
 
-Run server.js in the terminal and go to localhost://3000, then check it out!
+# db setup
+1) install rethink db on your machine: https://www.rethinkdb.com/docs/install/
+
+2) cd into project directory and run: `$ rethinkdb`. This will create a `rethinkdb_data` folder in your project dir and start the rethinkdb server.
+Go to `http://localhost:8080/#tables` to see the admin interface (looks cool).
+
+3) Run server.js in the terminal and go to localhost://3000, then check it out!
--- server.js
+++ server.js
@@ -1,31 +1,94 @@
-var express = require('express');
+const rows = 22;
+const cols = 22;
 
+// ---- DB SETUP ----+
+r = require('rethinkdb');
+const dBName = 'checkbox_db';
+const tableName = 'checkboxes';
+// create connection to db
+var connection = null;
+r.connect({host: 'localhost', port: 28015}, function (err, conn) {
+    if (err) throw err;
+    connection = conn;
+
+    resetBoxes();
+
+    r.db('test').tableList().run(connection, function (err, result) {
+        if (err) throw err;
+        // create table in db
+        if (result.indexOf(tableName) == -1) {
+            console.log('create table' + tableName);
+            r.db('test').tableCreate(tableName).run(connection, function (err, res) {
+                if (err) throw err;
+                console.log(res);
+                resetBoxes();
+            });
+        }
+    })
+});
+
+
+function resetBoxes() {
+    // ---- Reset all the boxes documents in db ----+
+    let boxes = [];
+    for (var i = 0; i < (cols * rows); i++) {
+        boxes.push({x: true, y: i})
+    }
+    console.log(boxes);
+    // remove all boxes from table
+    r.table(tableName).delete().run(connection, function (err, result) {
+        if (err) throw err;
+        console.log(JSON.stringify(result, null, 2));
+        // save boxes in table
+        r.table(tableName).insert(boxes).run(connection, function (err, result) {
+            if (err) throw err;
+            console.log(JSON.stringify(result, null, 2));
+        })
+    })
+
+}
+
+// ---- SOCKET IO SETUP ----+
+
+var express = require('express');
 var app = express();
 var server = app.listen(3000);
-
 app.use(express.static('public'));
 
 console.log('my socket server is running');
-
-
-
 var socket = require('socket.io');
-
 var io = socket(server);
-
 io.sockets.on('connection', newConnection);
 
-function newConnection(socket){
-  console.log('new connection: ' + socket.id);
+function newConnection(socket) {
+    // --- On new connection init client with data from the db ----+
+    console.log('new connection: ' + socket.id);
+    r.table(tableName).run(connection, function (err, cursor) {
+        if (err) throw err;
+        cursor.toArray(function (err, result) {
+            if (err) throw err;
+            console.log('init boxes');
+            result.map(initBoxes);
 
-  // 2) fire fuction if receive message with name mouse
-  socket.on('mouse', mouseMsg);
+            function initBoxes(data) {
+                socket.emit('mouse', data);
+            }
+        });
+    });
 
-  function mouseMsg(data){
-    // 3) take the message in !
-    socket.broadcast.emit('mouse',data);
-    //io.broadcast.emit('mouse',data);
-    console.log(data);
-  }
+
+    // 2) fire function if receive message with name mouse
+    socket.on('mouse', mouseMsg);
+
+    function mouseMsg(data) {
+        // 3) ---- Update the checkbox status in db and broadcast to other clients ----+
+        r.db('test').table('checkboxes').filter({y: data.y}).update({x: data.x}).run(connection, function (err, result) {
+            if (err) throw err;
+            // console.log(JSON.stringify(result, null, 2))
+        });
+
+        socket.broadcast.emit('mouse', data);
+        console.log(data);
+    }
 
 }
