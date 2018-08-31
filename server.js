64d27313e18b7510c25de1e8486cf9928a7693f2
Jonas Schell <jonasschell@ocupe.org>
2018-08-31 20:27:36 +0200

    cleanup and small improvements

--- server.js
+++ server.js
@@ -1,76 +1,75 @@
+// ---- Settings ----+
 const rows = 22;
 const cols = 22;
+const reset = true;  // If true -> boxes get reset on server start.
 
 // ---- DB SETUP ----+
 r = require('rethinkdb');
 const dBName = 'test';
 const tableName = 'checkboxes';
-// create connection to db
-var connection = null;
+
+// Create a connection to the database.
+let connection = null;
 r.connect({host: 'localhost', port: 28015}, function (err, conn) {
     if (err) throw err;
     connection = conn;
 
-
     r.db(dBName).tableList().run(connection, function (err, result) {
         if (err) throw err;
-        // create table in db
-        if (result.indexOf(tableName) == -1) {
-            console.log('create table: ' + tableName);
+
+        // If table does not exist -> create table in db
+        if (result.indexOf(tableName) === -1) {
+            console.log('Create table: ' + tableName);
             r.db(dBName).tableCreate(tableName).run(connection, function (err, res) {
                 if (err) throw err;
-                // console.log(res);
                 resetBoxes();
             });
         } else {
             // if table exist
-            console.log('reset table');
-            resetBoxes();
+            if (reset === true) {
+                console.log('Reset table: ' + tableName);
+                resetBoxes();
+            }
         }
     })
 });
 
 
 function resetBoxes() {
-    // ---- Reset all the boxes documents in db ----+
+    // ---- Reset all boxes in db ----+
     let boxes = [];
-    for (var i = 0; i < (cols * rows); i++) {
+    for (let i = 0; i < (cols * rows); i++) {
         boxes.push({x: true, y: i})
     }
-    // console.log(boxes);
     // remove all boxes from table
     r.table(tableName).delete().run(connection, function (err, result) {
         if (err) throw err;
-        // console.log(JSON.stringify(result, null, 2));
         // save boxes in table
         r.table(tableName).insert(boxes).run(connection, function (err, result) {
             if (err) throw err;
-            // console.log(JSON.stringify(result, null, 2));
         })
     })
-
 }
 
 // ---- SOCKET IO SETUP ----+
-
-var express = require('express');
-var app = express();
-var server = app.listen(3000);
+let express = require('express');
+let app = express();
+let server = app.listen(3000);
 app.use(express.static('public'));
 
-console.log('my socket server is running');
-var socket = require('socket.io');
-var io = socket(server);
-io.sockets.on('connection', newConnection);
+console.log('My socket server is running');
+let socket = require('socket.io');
+let io = socket(server);
+io.sockets.on('connection', onNewConnection);
 
-function newConnection(socket) {
-    // --- On new connection init client with data from the db ----+
-    console.log('new connection: ' + socket.id);
+function onNewConnection(socket) {
+    // --- 1) On new connection, init client with data from the db. ----+
+    console.log('New connection: ' + socket.id);
     r.table(tableName).run(connection, function (err, cursor) {
         if (err) throw err;
         cursor.toArray(function (err, result) {
             if (err) throw err;
-            console.log('init boxes');
+            console.log('Initialize boxes for socket: ' + socket.id);
             result.map(initBoxes);
 
             function initBoxes(data) {
@@ -80,18 +79,17 @@ function newConnection(socket) {
     });
 
 
-    // 2) fire function if receive message with name mouse
+    // ---- 2) On message from client -> fire function. ----+
     socket.on('mouse', mouseMsg);
 
     function mouseMsg(data) {
-        // 3) ---- Update the checkbox status in db and broadcast to other clients ----+
+        // ---- 3) Update the checkbox status in db ----+
         r.db(dBName).table('checkboxes').filter({y: data.y}).update({x: data.x}).run(connection, function (err, result) {
             if (err) throw err;
-            // console.log(JSON.stringify(result, null, 2))
+
+            // 4) ---- Broadcast update to other clients ----+
+            console.log(data);
+            socket.broadcast.emit('mouse', data);
         });
-
-        socket.broadcast.emit('mouse', data);
-        console.log(data);
     }
-
 }
